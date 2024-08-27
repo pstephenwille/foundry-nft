08ec50133018c18cec397932c5a2d36b62c8fb9e
stephen.wille <stephen.wille@climate.com>
2024-08-26 21:04:16 -0700

    test

--- src/FoundryNFT.sol
+++ src/FoundryNFT.sol
@@ -2,13 +2,14 @@
 pragma solidity ^0.8.13;
 
 import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "forge-std/Test.sol";
 
 contract FoundryNFT is ERC721 {
-    uint256 public totalSupply = 0;
-    address public owner;
+    uint256 public totalSupply = 1;
+    address payable public  owner;
 
     constructor() ERC721("FoundryNFT", "XFNFT") {
-        owner = msg.sender;
+        owner = payable(msg.sender);
     }
 
     function mint() external payable {
@@ -17,4 +18,11 @@ contract FoundryNFT is ERC721 {
         _mint(msg.sender, totalSupply);
     }
 
+    function mytransfer() public  {
+        approve(msg.sender, totalSupply);
+        (bool ok, )=   address(msg.sender).call{value: totalSupply}("");
+        console.log('ok', ok);
+    }
+
+    receive() external payable {}
 }
--- test/FoundryNFT.t.sol
+++ test/FoundryNFT.t.sol
@@ -1,29 +1,53 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
+import "forge-std/Test.sol";
 import {FoundryNFT} from "../src/FoundryNFT.sol";
 
 contract FoundryNFTTest is Test {
     FoundryNFT public sut;
+    event Transfer(address from, address to, uint tokenId);
 
     function setUp() public {
         sut = new FoundryNFT();
+        vm.deal(sut.owner(), 3 ether);
+        address(sut.owner()).call{value: 3 ether}('');
     }
 
-    function test_TokenNameIsFoundryNFT() public {
+    function xtest_TokenNameIsFoundryNFT() public {
         string memory name = sut.name();
         assertEq('FoundryNFT', name);
     }
 
-    function test_totalSupplyIsInremented() public {
+    function xtest_totalSupplyIsInremented() public {
         assertEq(sut.totalSupply(), 0);
         sut.mint();
         assertEq(sut.totalSupply(), 1);
         assertEq(sut.balanceOf(sut.owner()), 1);
     }
 
-    function test_symbol() public {
+    function xtest_symbol() public {
         assertEq(sut.symbol(), 'XFNFT');
     }
+
+    function xtest_mint() public {
+        address badAddr = address(0xBad);
+        vm.expectEmit();
+        emit Transfer(address(this), sut.owner(), 2);
+//        vm.deal(sut.owner(), 10 ether);
+
+//        sut.mint();
+//        sut.mint();
+        sut.mint();
+        vm.prank(badAddr);
+        sut.mytransfer();
+        console.log(sut.balanceOf(badAddr));
+    }
+
+    function test_test() public {
+        sut.mytransfer();
+        console.log('1', sut.balanceOf(sut.owner()));
+        console.log(address(sut.owner()).balance);
+    }
+    receive() external payable {}
 }
